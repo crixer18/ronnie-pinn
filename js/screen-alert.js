914e86fbbd33213c8ec4ac54531ff3a05c96b313
crixer18 <crixer18@xn--59g>
2021-02-07 12:21:39 +0100

    home

---
+++ js/screen-alert.js
@@ -0,0 +1,7 @@
+
+  if (window.innerWidth < 1000){
+    window.alert('Per goderti appieno questa storia, ti cognsiglio di navigare il sito da uno schermo più grande.');
+
+    var bar = document.getElementById('messaggio');
+    bar.innerHTML = '<div id="alert"><p>Per goderti appieno questa storia, ti cognsiglio di navigare il sito da uno schermo più grande.</p></div>';
+  }
